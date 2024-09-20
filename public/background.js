af8fce5e308694633d1300d85b9d75aaf1bbe210
Qi Xiang <qixyqix@outlook.com>
2024-09-20 20:56:06 +0800

    Added ability to identify lyric divs in spotify web player

--- public/background.js
+++ public/background.js
@@ -1,1 +1,22 @@
-console.log('hello!');
+const lyricElementSelector = '[data-testid="fullscreen-lyric"]';
+
+const observer = new MutationObserver(() => {
+    if (document.querySelector(lyricElementSelector)) {
+        console.log('Lyric elements found')
+        init();
+        observer.disconnect()
+    }
+})
+
+observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true })
+
+function init() {
+    const lyricElements = Array.from(document.querySelectorAll(lyricElementSelector))
+    for(let lyricElement of lyricElements){
+
+        let lyricTextDiv = lyricElement.childNodes[0]
+        // console.dir(lyricElement)
+        console.log(lyricTextDiv.innerText)
+    }
+    // console.log(lyricElements);
+}

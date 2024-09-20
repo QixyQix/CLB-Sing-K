e9ad4f95f2e639c5bf56bb8f40f1b958c7459357
Qi Xiang <qixyqix@outlook.com>
2024-09-21 01:25:23 +0800

    Added ability to convert traditional chinese to simplified chinese

--- public/t2s.js
+++ public/t2s.js
@@ -1,33 +1,3 @@
-const lyricElementSelector = '[data-testid="fullscreen-lyric"]';
-
-const observer = new MutationObserver(() => {
-    if (document.querySelector(lyricElementSelector)) {
-        console.log('Lyric elements found')
-        init();
-        observer.disconnect()
-    }
-})
-observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true })
-
-function init() {
-    const lyricElements = Array.from(document.querySelectorAll(lyricElementSelector))
-    for(let lyricElement of lyricElements){
-        let lyricTextDiv = lyricElement.childNodes[0]
-        let lyricText = lyricTextDiv.innerText
-        let simplifiedText = ""
-        for(let char of lyricText){
-            simplifiedText += tradToSimp[char] ? tradToSimp[char] : char
-        }
-        console.log(lyricTextDiv.innerText)
-        lyricTextDiv.innerText = simplifiedText;
-        console.log(lyricTextDiv.innerText)
-        console.log("---")
-    }
-    // console.log(lyricElements);
-}
-
-// TODO: Figure out how to import this
-
 const tradToSimp = {
     "㑮": "𫝈",
     "㑯": "㑔",
@@ -6945,3 +6915,4 @@ const tradToSimp = {
     "𱋂": "𪋈",
     "𱵭": "𮵚"
 };
+module.exports = tradToSimp

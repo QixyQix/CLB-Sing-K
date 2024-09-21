f23fd0b1a07c3d5a4cda55ad09136fcea6310cde
Qi Xiang <qixyqix@outlook.com>
2024-09-21 18:52:41 +0800

    Update chrome extension index page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>我也要唱K</h1>
       <h2>CLB Sing K</h2>
-      <p>Chrome extension to covnert traditional chinese lyrics to simplified chinese, and provide pinyin romanization</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <p>Chrome extension to convert traditional chinese lyrics to simplified chinese, and provide pīnyīn, for Spotify's web player</p>
+      <p>Made with ❤️ by <a href='https://github.com/QixyQix' target='_blank'>Qi Xiang</a></p>
     </>
   )
 }

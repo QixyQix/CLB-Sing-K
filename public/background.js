5cef4448b33204b7c11497a2a2dd33e5998b1ee6
Qi Xiang <qixyqix@outlook.com>
2024-09-21 17:51:12 +0800

    Added ability for lyrics to auto convert when next song plays
    TODO: Fix when a 'You'll have to guess the lyrics for this one' page appears, next song lyrics don't get converted

--- public/background.js
+++ public/background.js
@@ -1,4 +1,6 @@
 const lyricElementSelector = '[data-testid="fullscreen-lyric"]';
+let overallParentDiv = undefined;
+
 
 const observer = new MutationObserver(() => {
     if (document.querySelector(lyricElementSelector)) {
@@ -9,8 +11,21 @@ const observer = new MutationObserver(() => {
 })
 observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true })
 
+const childListObserver = new MutationObserver((mutationList) => {
+    for(let mutation of mutationList){
+        if (mutation.type === 'childList'){
+            childListObserver.disconnect();
+            init()
+            break;
+        }
+    }
+})
+
 function init() {
     const lyricElements = Array.from(document.querySelectorAll(lyricElementSelector))
+    if(!overallParentDiv){
+        overallParentDiv = lyricElements[0].parentNode.parentNode;
+    }
     for (let lyricElement of lyricElements) {
 
         let lyricTextDiv = lyricElement.childNodes[0];
@@ -28,9 +43,13 @@ function init() {
         lyricTextDiv.innerHTML = pinyinText.trim() !== "" ? `<span style="font-size:1.4rem;line-height:0.5rem">${pinyinText}</span><br/>${simplifiedText}` :  simplifiedText;
 
 
-        console.log(lyricText);
-        console.log(simplifiedText);
-        console.log(pinyinText);
-        console.log("---");
+        // console.log(lyricText);
+        // console.log(simplifiedText);
+        // console.log(pinyinText);
+        // console.log("---");
     }
+    if(overallParentDiv){
+        childListObserver.observe(overallParentDiv, { attributes: false, childList: true, characterData: false, subtree: true })
+    }
+    console.log('Lyrics Initialised')
 }

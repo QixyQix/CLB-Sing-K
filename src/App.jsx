48078e8d58c5dc746dd958420776ead890c91ae8
Qi Xiang <qixyqix@outlook.com>
2024-09-23 22:44:58 +0800

    Added ability to save settings in chrome storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { SelectInput } from './SelectInput'
 
@@ -11,6 +11,26 @@ function App() {
   const [pinyinSetting, setPinyinSetting] = useState("pinyin-small")
   const [pinyinPrefSetting, setPinyinPrefSetting] = useState("pinyin-pref-cn")
 
+  useEffect(() => {
+    console.log("Loading Settings for CLB Sing K")
+    chrome.storage.sync.get('clbSingKSettings', (settings) => {
+      settings = settings.clbSingKSettings;
+
+      let loadedCharSetting = settings["char"] ? settings["char"] : "char-simplified";
+      let loadedPinyinSetting = settings["pinyin"] ? settings["pinyin"] : "pinyin-small";
+      let loadedPinyinPrefSetting = settings["pinyinPref"] ? settings["pinyinPref"] : "pinyin-pref-cn"
+
+      setCharSetting(loadedCharSetting)
+      setPinyinSetting(loadedPinyinSetting)
+      setPinyinPrefSetting(loadedPinyinPrefSetting)
+    })
+  }, []);
+
+  useEffect(() => {
+    const settingObj = { char: charSetting, pinyin: pinyinSetting, pinyinPref: pinyinPrefSetting }
+    chrome.storage.sync.set({ clbSingKSettings: settingObj })
+  }, [charSetting, pinyinSetting, pinyinPrefSetting]);
+
   return (
     <>
       <h1>我也要唱K</h1>
@@ -18,17 +38,17 @@ function App() {
       <p>Chrome extension to convert traditional chinese lyrics to simplified chinese, and provide pīnyīn, for Spotify's web player</p>
 
       <hr />
+
       <h3>Settings</h3>
       <form className='form-centered'>
-        <SelectInput value={charSetting} onChange={setCharSetting} label="Chinese Character" options={charOptions} valueKey="value" displayKey="title"/>
-        <SelectInput value={pinyinSetting} onChange={setPinyinSetting} label="Pinyin" options={pinyinOptions} valueKey="value" displayKey="title"/>
-        <SelectInput value={pinyinPrefSetting} onChange={setPinyinPrefSetting}label="Pinyin Preference" options={pinyinPrefOptions} valueKey="value" displayKey="title"/>
+        <SelectInput value={charSetting} onChange={setCharSetting} label="Chinese Character" options={charOptions} valueKey="value" displayKey="title" />
+        <SelectInput value={pinyinSetting} onChange={setPinyinSetting} label="Pinyin" options={pinyinOptions} valueKey="value" displayKey="title" />
+        <SelectInput value={pinyinPrefSetting} onChange={setPinyinPrefSetting} label="Pinyin Preference" options={pinyinPrefOptions} valueKey="value" displayKey="title" />
       </form>
 
       <hr />
 
       <p>Made with ❤️ by <a href='https://github.com/QixyQix' target='_blank'>Qi Xiang</a></p>
-
     </>
   )
 }
